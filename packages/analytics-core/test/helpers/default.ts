7b76488b3b6e58049041cad1cdef11883b8f8db5
joowon-dm-snu <joowon.kim@dm.snu.ac.kr>
2023-01-18 15:47:02 +0900

    test: setup test

---
+++ packages/analytics-core/test/helpers/default.ts
@@ -0,0 +1,45 @@
+import { Config } from '@coxwave/analytics-types';
+
+import { getDefaultConfig } from '../../src/config';
+
+export const useDefaultConfig = (): Config => ({
+  projectToken: PROJECT_TOKEN,
+  transportProvider: {
+    send: () => Promise.resolve(null),
+  },
+  storageProvider: {
+    isEnabled: async () => true,
+    get: async () => undefined,
+    set: async () => undefined,
+    remove: async () => undefined,
+    reset: async () => undefined,
+    getRaw: async () => undefined,
+  },
+  ...getDefaultConfig(),
+});
+
+export const PROJECT_TOKEN = 'projectToken';
+export const USER_ID = 'userId';
+export const DEVICE_ID = 'deviceId';
+
+/*
+There is no way to figure out the state of a promise with normal API.
+This helper expose the state of a promise via race.
+https://stackoverflow.com/a/35820220
+
+Example:
+const a = Promise.resolve();
+const b = Promise.reject();
+const c = new Promise(() => {});
+
+promiseState(a).then(state => console.log(state)); // fulfilled
+promiseState(b).then(state => console.log(state)); // rejected
+promiseState(c).then(state => console.log(state)); // pending
+*/
+export function promiseState(p: Promise<any>) {
+  const t = {};
+  return Promise.race([p, t]).then(
+    (v) => (v === t ? 'pending' : 'fulfilled'),
+    () => 'rejected',
+  );
+}
