20488044ad863d8ec587bd6b33b1c0ab66b7413c
joowon-dm-snu <joowon.kim@dm.snu.ac.kr>
2023-01-19 15:06:56 +0900

    fix: extend-name for test

--- packages/analytics-client-common/src/cookie-name.ts
+++ packages/analytics-client-common/src/cookie-name.ts
@@ -1,5 +1,5 @@
 import { COXWAVE_PREFIX } from '@coxwave/analytics-core';
 
-export const getCookieName = (projectToken: string, postKey = '', limit = 10) => {
+export const getCookieName = (projectToken: string, postKey = '', limit = 15) => {
   return [COXWAVE_PREFIX, postKey, projectToken.substring(0, limit)].filter(Boolean).join('_');
 };
